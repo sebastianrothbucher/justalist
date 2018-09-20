bf5d1320d47d4afb7c84c253cf090cca610cc1d1
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2018-09-20 21:37:39 +0200

    Initial CDN

---
+++ config/webpack.own.prodcdn.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
+
+// call per se node_modules/.bin/webpack --config config/webpack.own.prod.js (aka npm run buildownprod)
+// can be debugged: node --inspect-brk node_modules/.bin/webpack --config config/webpack.own.dev.js - then: open devtools, click node, console: require('loadertodebug'), it appears in sources, set breakpoint & GO ;-)
+module.exports = { // TODO: require prod as basis
+    entry: {
+        app: path.join(__dirname, "../src/index.js"),
+        vendor: [
+            'react-scripts/config/polyfills' /* 1st; & thx, FB */, 
+            'prop-types',
+            'react-dom',
+            'react-redux',
+            'redux',
+            'redux-thunk',
+            'immutable',
+            'd3-color',
+        ],
+    },
+    externals: {
+        'react': 'React'
+    },
+    output: {
+        path: path.join(__dirname, "../build"),
+        filename: '[name].[hash:8].js',
+        pathinfo: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: require.resolve("babel-loader"),
+                options: {
+                    cacheDirectory: true,
+                    presets: [
+                        "react", 
+                        "env",
+                    ],
+                    plugins: [
+                        "transform-object-rest-spread", 
+                        "babel-plugin-transform-react-jsx-source",
+                        "babel-plugin-transform-react-jsx-self",
+                    ],
+                },
+            }, 
+            {
+                test: /\.css$/,
+                include: path.join(__dirname, "../src"),
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader", 
+                    use: {
+                        loader: require.resolve("css-loader"),
+                        options: {
+                            minimize: true,
+                            sourceMap: true,
+                            modules: true,
+                        },
+                    },
+                }),
+            }, 
+            {
+                test: /\.css$/,
+                exclude: path.join(__dirname, "../src"),
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader", 
+                    use: {
+                        loader: require.resolve("css-loader"),
+                        options: {
+                            minimize: true,
+                            sourceMap: true,
+                        },
+                    },
+                }),
+            }, 
+            {
+                test: /\.(ttf|woff2|woff|eot)$/, // TODO: load via CDN also
+                loader: require.resolve("file-loader"),
+                options: {
+                    name: "[name].[hash:8].[ext]",
+                },
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                loader: require.resolve("url-loader"),
+                options: {
+                    limit: 20000,
+                    falback: "file-loader",
+                    name: "[name].[hash:8].[ext]", // used in fallback!
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin({
+            filename: "[name].[contenthash:8].css"
+        }),
+        new HtmlWebpackPlugin({
+            //title: "Test",
+            inject: "body", // implicit "onload" (following FB 4 now)
+            template: "public/indexcdn.html",
+        }),
+        new UglifyjsWebpackPlugin({
+            comments: false,
+            sourceMap: true,
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify("production"),
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+
+        }),
+    ],
+    devtool: "nosources-source-map", // only stack traces
+};
